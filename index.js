0e57e8707835c9fe24e44592e783e7031ad3fa17
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-11-02 01:21:59 +0530

    added all files.
    Completed the responsive design.

---
+++ index.js
@@ -0,0 +1,47 @@
+function myFunction(x) {
+    if (x.type == "email") {
+        let email = document.getElementById("signin--email").className = "hrStyle"
+        document.getElementById("signin--emailHeading").innerText = "Email address"
+    }
+    else {
+        let password = document.getElementById("signin--password").className = "hrStyle"
+        document.getElementById("signin--passHeading").innerText = "Password"
+    }
+    x.placeholder = "";
+    return
+}
+
+function myFunction1(x) {
+    if (x.type == "email") {
+        x.placeholder = "Email address"
+
+    }
+    else {
+        x.placeholder = "Password"
+    }
+    document.getElementById("signin--password").classList.remove("hrStyle")
+    document.getElementById("signin--email").classList.remove("hrStyle")
+    document.getElementById("signin--passHeading").innerText = ""
+    document.getElementById("signin--emailHeading").innerText = ""
+
+    return
+}
+
+function removeEye() {
+
+    let type = document.getElementById("signin--passwordInput").type
+    let type1
+    let eye
+    type == "password" ? type1 = "Text" : type1 = "password"
+    type == "password" ? eye = "./images/hide.png" : eye = "./images/view.png"
+    document.getElementById("signin--passwordInput").type = type1
+    document.getElementById("password--eye").src = eye
+}
+
+function support() {
+    alert("Please contact with support team.")
+}
+function signIn() {
+    alert("Sign in successfully")
+}
+
